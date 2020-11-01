1fd1b371fb8fe0021a0e0ee0721995113bfcbae6
Muratsoyler <soylermurat83@gmail.com>
2020-11-01 14:21:52 +0200

    Optifine.installer.main.java

---
+++ Optifine.installer.main.java
@@ -0,0 +1,11 @@
+package com.aof.mcinabox.launcher.optifine;
+//version 1.0
+
+ import OptiFine_1.12.2_HD_U_F5.jar;
+
+case 3;
+//if import fails
+
+Toast.makeText(MainActivity.CURRENT_ACTIVITY, MainActivity.CURRENT_ACTIVITY.getString(R.string.tips_Optifine_installation_failed), Toast.LENGTH_SHORT).show();
+mDialong.dismiss();
+//unfinished code dont judge on it but you can still tell me whats wrong with the code to help
